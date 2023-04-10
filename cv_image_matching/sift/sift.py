1e87886c740d564d9e43fa278576b4e712d02680
Jose Costa <melocosta.joselucas@gmail.com>
2023-04-10 10:46:34 +0200

    Start refactoring

--- cv_image_matching/sift/sift.py
+++ cv_image_matching/sift/sift.py
@@ -19,13 +19,36 @@ class SIFT:
         initial_sigma: float = 1.6,
         n_scales_per_octave: int = 3,
         n_octaves: int = 4,
+        assumed_blur: float = 0.5,
+        kp_find_threshold: float = 1e-3,
+        kp_max_tolerance: int = 1,
+        local_max_threshold: float = 5.0,
+        gaussian_window_histogram: float = 1.5,
+        num_bins_histogram: int = 36,
+        ksize_smooth_histogram: int = 5,
+        std_smooth_histogram: float = 1,
+        size_factor: float = 5,
+        n_spacial_bins: int = 4,
+        n_orientation_bins: int = 8,
+        f_max: float = 0.2,
+        f_scale: float = 512,
     ) -> None:
         self.initial_sigma = initial_sigma
         self.n_scales_per_octave = n_scales_per_octave
         self.n_octaves = n_octaves
-        self.keypoins = None
-        self.keypoints_cv = None
-        self.filtered_keypoints = None
+        self.assumed_blur = assumed_blur
+        self.kp_find_threshold = kp_find_threshold
+        self.kp_max_tolerance = kp_max_tolerance
+        self.local_max_threshold = local_max_threshold
+        self.gaussian_window_histogram = gaussian_window_histogram
+        self.num_bins_histogram = num_bins_histogram
+        self.ksize_smooth_histogram = ksize_smooth_histogram
+        self.std_smooth_histogram = std_smooth_histogram
+        self.size_factor = size_factor
+        self.n_spacial_bins = n_spacial_bins
+        self.n_orientation_bins = n_orientation_bins
+        self.f_max = f_max
+        self.f_scale = f_scale
 
     def compute_gaussian_scales(self) -> ndarray:
         """Create a list of gaussian kernels for each octave and scale"""
@@ -43,12 +66,16 @@ class SIFT:
 
         return self.gaussian_scales
 
-    def gaussian_images(self, image: np.ndarray) -> ndarray:
+    def gaussian_images(self) -> ndarray:
         """Create a stack of blurred images for each octave and scale.
 
         Args:
             image (np.ndarray): Input image.
         """
+
+        if not hasattr(self, "base_image"):
+            base_image_error = "Please set the base image first"
+            raise AttributeError(base_image_error)
 
         if not hasattr(self, "gaussian_scales"):
             self.compute_gaussian_scales()
@@ -58,24 +85,26 @@ class SIFT:
         for octave in range(self.n_octaves):
             gaussianed_images[octave] = {}
 
-            gaussianed_images[octave][0] = image
+            gaussianed_images[octave][0] = self.base_image
             for idx, scale in enumerate(self.gaussian_scales[1:]):
-                new_image = GaussianBlur(image, (0, 0), sigmaX=scale, sigmaY=scale)
+                new_image = GaussianBlur(
+                    self.base_image,
+                    (0, 0),
+                    sigmaX=scale,
+                    sigmaY=scale,
+                )
                 gaussianed_images[octave][idx + 1] = new_image
 
-            image = resize(
+            self.base_image = resize(
                 gaussianed_images[octave][idx - 2],
-                (image.shape[1] // 2, image.shape[0] // 2),
+                (self.base_image.shape[1] // 2, self.base_image.shape[0] // 2),
             )
+
+        self.gaussianed_images = gaussianed_images
 
         return gaussianed_images
 
-    def generate_base_image(
-        self,
-        image: np.ndarray,
-        sigma: float,
-        assumed_blur: float,
-    ) -> np.ndarray:
+    def generate_base_image(self, image: np.ndarray) -> np.ndarray:
         """Upsamples the input image by 2 in both directions
         and applies Gaussian blur to generate a base image.
 
@@ -91,30 +120,37 @@ class SIFT:
 
         """
         upscaled_image = resize(image, (0, 0), fx=2, fy=2, interpolation=INTER_LINEAR)
-        sigma_diff = np.sqrt(max(sigma**2 - (2 * assumed_blur) ** 2, 0.01))
+        sigma_diff = np.sqrt(
+            max(self.initial_sigma**2 - (2 * self.assumed_blur) ** 2, 0.01),
+        )
         base_image = GaussianBlur(
             upscaled_image,
             (0, 0),
             sigmaX=sigma_diff,
             sigmaY=sigma_diff,
         )
-
+        self.base_image = base_image
         return base_image
 
-    def compute_dog_images(self, gaussianed_images: ndarray) -> ndarray:
+    def compute_dog_images(self) -> ndarray:
         """Compute the difference of gaussian images for each octave and scale.
 
         Args:
             gaussian_images (ndarray): Stack of blurred images for each octave and scale
         """
+        if not hasattr(self, "gaussianed_images"):
+            gaussianed_images_error = "Please generate the gaussianed images first"
+            raise AttributeError(gaussianed_images_error)
+
         dog_images = {}
         for octave in range(self.n_octaves):
             dog_images[octave] = {}
             for scale in range(self.n_intervals - 1):
                 dog_images[octave][scale] = subtract(
-                    gaussianed_images[octave][scale + 1],
-                    gaussianed_images[octave][scale],
+                    self.gaussianed_images[octave][scale + 1],
+                    self.gaussianed_images[octave][scale],
                 )
+        self.dog_images = dog_images
         return dog_images
 
     def is_keypoint(
@@ -162,12 +198,41 @@ class SIFT:
                                 return False
         return True
 
-    def find_keypoints(
-        self,
-        dog_images: ndarray,
-        threshold: float = 0.01,
-        max_tolerance: int = 1,
-    ) -> ndarray:
+    def _is_keypoint(self, octave: int, scale: int, i: int, j: int) -> bool:
+        """Check if a pixel is a keypoint (local maximum)."""
+
+        center_pixel = self.dog_images[octave][scale][i][j]
+
+        if abs(center_pixel) < self.kp_find_threshold:
+            return False
+
+        count_fails = 0
+        for x in np.arange(-1, 2):
+            for y in np.arange(-1, 2):
+                for z in np.arange(-1, 2):
+                    bound_x, bound_y = self.dog_images[octave][scale + z].shape
+                    if 0 <= i + x < bound_x and 0 <= j + y < bound_y:
+                        if x == 0 and y == 0 and z == 0:
+                            continue
+                        if (
+                            center_pixel > 0
+                            and center_pixel
+                            < self.dog_images[octave][scale + z][i + x][j + y]
+                        ):
+                            count_fails += 1
+                            if count_fails > self.kp_max_tolerance:
+                                return False
+                        if (
+                            center_pixel < 0
+                            and center_pixel
+                            > self.dog_images[octave][scale + z][i + x][j + y]
+                        ):
+                            count_fails += 1
+                            if count_fails > self.kp_max_tolerance:
+                                return False
+        return True
+
+    def find_keypoints(self, dog_images: ndarray) -> ndarray:
         """Find the keypoints in the difference of gaussian images.
 
         Args:
@@ -175,69 +240,74 @@ class SIFT:
             for each octave and scale.
         """
         keypoints_cv = []
-        keypoints = []
-        # print("n_octaves: ", self.n_octaves)
-        # print("n_scales_per_octave: ", self.n_scales_per_octave)
+        keypoints_raw = []
         for octave in range(self.n_octaves):
             for scale in range(1, self.n_scales_per_octave):
-                # print("i range: ", dog_images[octave][scale].shape[0])
-                # print("j range: ", dog_images[octave][scale].shape[1])
-                # print("dog shape: ", dog_images[octave][scale].shape)
                 for i in range(dog_images[octave][scale].shape[0]):
                     for j in range(dog_images[octave][scale].shape[1]):
-                        if self.is_keypoint(
-                            dog_images,
-                            octave,
-                            scale,
-                            i,
-                            j,
-                            threshold,
-                            max_tolerance,
-                        ):
-                            bound_x, bound_y = dog_images[octave][scale].shape
-                            new_i, new_j = i, j
-                            if not (
-                                i - 1 < 0
-                                or i + 1 >= bound_x
-                                or j - 1 < 0
-                                or j + 1 >= bound_y
-                            ):
-                                hessian_2d, hessian_3d, grad = self.calculate_hessian(
-                                    dog_images,
-                                    octave,
-                                    scale,
-                                    i,
-                                    j,
-                                )
-
-                                d = -1 * np.dot(np.linalg.inv(hessian_3d), grad)
-
-                                new_i = new_i + min(1, max(-1, np.round(d[0])))
-                                new_j = new_j + min(1, max(-1, np.round(d[1])))
-
-                            keypoint = KeyPoint()
-                            keypoint.pt = (
-                                new_j * (2**octave),
-                                new_i * (2**octave),
-                            )
-                            keypoint.octave = octave + scale * (2**8)
-                            keypoint.size = (
-                                self.initial_sigma
-                                * (2 ** (scale / float32(self.n_scales_per_octave)))
-                                * (2 ** (octave + 1))
-                            )
+                        if self._is_keypoint(octave, scale, i, j):
+                            new_i, new_j = self._check_new_pos(octave, scale, i, j)
+                            keypoint = self._create_kp(octave, scale, new_i, new_j)
                             keypoints_cv.append(keypoint)
-                            keypoints.append((octave, scale, i, j))
-        return keypoints_cv, keypoints
+                            keypoints_raw.append((octave, scale, i, j))
+        self.keypoints = keypoints_cv
+        self.keypoints_raw = keypoints_raw
+        return keypoints_cv, keypoints_raw
 
-    def calculate_hessian(
+    def _create_kp(
         self,
-        dog_images: ndarray,
         octave: int,
         scale: int,
         i: int,
         j: int,
-    ) -> ndarray:
+        orientation: float = None,
+        response: float = None,
+    ):
+        """Create a keypoint object from the given parameters."""
+        keypoint = KeyPoint()
+        keypoint.response = response
+        keypoint.pt = (
+            j * (2**octave),
+            i * (2**octave),
+        )
+        keypoint.octave = octave + scale * (2**8)
+        keypoint.angle = orientation
+        keypoint.size = (
+            self.initial_sigma
+            * (2 ** (scale / float32(self.n_scales_per_octave)))
+            * (2 ** (octave + 1))
+        )
+
+        return keypoint
+
+    def _check_new_pos(self, octave: int, scale: int, i: int, j: int):
+        """Check if the new position is within the image bounds + refine"""
+        bound_x, bound_y = self.dog_images[octave][scale].shape
+        new_i, new_j = i, j
+        if not (i - 1 < 0 or i + 1 >= bound_x or j - 1 < 0 or j + 1 >= bound_y):
+            new_i, new_j = self._evaluate_new_pos(
+                octave,
+                scale,
+                i,
+                j,
+            )
+
+        return new_i, new_j
+
+    def _evaluate_new_pos(self, octave: int, scale: int, i: int, j: int):
+        """Refine the new position of the keypoint."""
+        bound_x, bound_y = self.dog_images[octave][scale].shape
+        if i - 1 < 0 or i + 1 >= bound_x or j - 1 < 0 or j + 1 >= bound_y:
+            return i, j
+        _, hessian_3d, grad = self.calculate_hessian(octave, scale, i, j)
+
+        d = -1 * np.dot(np.linalg.inv(hessian_3d), grad)
+
+        new_i = i + min(1, max(-1, np.round(d[0])))
+        new_j = j + min(1, max(-1, np.round(d[1])))
+        return new_i, new_j
+
+    def calculate_hessian(self, octave: int, scale: int, i: int, j: int) -> ndarray:
         """Calculate the hessian matrix for a given pixel.
 
         Args:
@@ -254,37 +324,37 @@ class SIFT:
         # print()
 
         d_xx = (
-            dog_images[octave][scale][i - 1][j]
-            - 2 * dog_images[octave][scale][i][j]
-            + dog_images[octave][scale][i - 1][j]
+            self.dog_images[octave][scale][i - 1][j]
+            - 2 * self.dog_images[octave][scale][i][j]
+            + self.dog_images[octave][scale][i - 1][j]
         )
         d_yy = (
-            dog_images[octave][scale][i][j - 1]
-            - 2 * dog_images[octave][scale][i][j]
-            + dog_images[octave][scale][i][j + 1]
+            self.dog_images[octave][scale][i][j - 1]
+            - 2 * self.dog_images[octave][scale][i][j]
+            + self.dog_images[octave][scale][i][j + 1]
         )
         d_xy = (1 / 4) * (
-            dog_images[octave][scale][i + 1][j + 1]
-            - dog_images[octave][scale][i - 1][j + 1]
-            - dog_images[octave][scale][i + 1][j - 1]
-            + dog_images[octave][scale][i - 1][j - 1]
+            self.dog_images[octave][scale][i + 1][j + 1]
+            - self.dog_images[octave][scale][i - 1][j + 1]
+            - self.dog_images[octave][scale][i + 1][j - 1]
+            + self.dog_images[octave][scale][i - 1][j - 1]
         )
         d_ss = (
-            dog_images[octave][scale - 1][i][j]
-            - 2 * dog_images[octave][scale][i][j]
-            + dog_images[octave][scale + 1][i][j]
+            self.dog_images[octave][scale - 1][i][j]
+            - 2 * self.dog_images[octave][scale][i][j]
+            + self.dog_images[octave][scale + 1][i][j]
         )
         d_xs = (1 / 4) * (
-            dog_images[octave][scale + 1][i + 1][j]
-            - dog_images[octave][scale + 1][i - 1][j]
-            - dog_images[octave][scale - 1][i + 1][j]
-            + dog_images[octave][scale - 1][i - 1][j]
+            self.dog_images[octave][scale + 1][i + 1][j]
+            - self.dog_images[octave][scale + 1][i - 1][j]
+            - self.dog_images[octave][scale - 1][i + 1][j]
+            + self.dog_images[octave][scale - 1][i - 1][j]
         )
         d_ys = (1 / 4) * (
-            dog_images[octave][scale + 1][i][j + 1]
-            - dog_images[octave][scale + 1][i][j - 1]
-            - dog_images[octave][scale - 1][i][j + 1]
-            + dog_images[octave][scale - 1][i][j - 1]
+            self.dog_images[octave][scale + 1][i][j + 1]
+            - self.dog_images[octave][scale + 1][i][j - 1]
+            - self.dog_images[octave][scale - 1][i][j + 1]
+            + self.dog_images[octave][scale - 1][i][j - 1]
         )
         hessian_2d = np.array([[d_xx, d_xy], [d_xy, d_yy]])
         hessian_3d = np.array(
@@ -293,12 +363,12 @@ class SIFT:
 
         grad = 0.5 * np.array(
             [
-                dog_images[octave][scale][i + 1][j]
-                - dog_images[octave][scale][i - 1][j],
-                dog_images[octave][scale][i][j + 1]
-                - dog_images[octave][scale][i][j - 1],
-                dog_images[octave][scale + 1][i][j]
-                - dog_images[octave][scale - 1][i][j],
+                self.dog_images[octave][scale][i + 1][j]
+                - self.dog_images[octave][scale][i - 1][j],
+                self.dog_images[octave][scale][i][j + 1]
+                - self.dog_images[octave][scale][i][j - 1],
+                self.dog_images[octave][scale + 1][i][j]
+                - self.dog_images[octave][scale - 1][i][j],
             ],
         )
 
@@ -324,10 +394,9 @@ class SIFT:
         """
         bound_x, bound_y = dog_images[octave][scale].shape
         if i - 1 < 0 or i + 1 >= bound_x or j - 1 < 0 or j + 1 >= bound_y:
-            return False, None, None, None
+            return False
 
         hessian_2d, hessian_3d, grad = self.calculate_hessian(
-            dog_images,
             octave,
             scale,
             i,
@@ -337,26 +406,41 @@ class SIFT:
         det_hessian = np.linalg.det(hessian_2d)
 
         if det_hessian <= 0:
-            return False, None, None, None
-
+            return False
         trace_hessian = np.trace(hessian_2d)
 
         a = (trace_hessian**2) / det_hessian
         a_max = (threshold + 1) ** 2 / threshold
-        d = -1 * np.dot(np.linalg.inv(hessian_3d), grad)
-        response = dog_images[octave][scale][i][j] - 0.5 * np.dot(grad, d)
-        # print("Hessian: ", hessian)
-        # print("det_hessian: ", det_hessian)
-        # print("trace hessian: ", trace_hessian)
-        # print("a: ", a)
-        # print("a_max: ", a_max)
 
-        return a < a_max, i, j, response
+        return a < a_max
+
+    def _get_position(self, keypoint: KeyPoint) -> tuple[int, int]:
+        """Get the position of the keypoint in the original image."""
+        scale, octave = divmod(keypoint.octave, 2**8)
+        scale = int(scale)
+        y = int(keypoint.pt[0] / (2**octave))
+        x = int(keypoint.pt[1] / (2**octave))
+        return (octave, scale, x, y)
+
+    def _calculate_pixel_response(self, octave, scale, i, j):
+        bound_x, bound_y = self.dog_images[octave][scale].shape
+        if i - 1 < 0 or i + 1 >= bound_x or j - 1 < 0 or j + 1 >= bound_y:
+            return 0
+
+        _, hessian_3d, grad = self.calculate_hessian(
+            octave,
+            scale,
+            i,
+            j,
+        )
+
+        d = -1 * np.dot(np.linalg.inv(hessian_3d), grad)
+        response = self.dog_images[octave][scale][i][j] - 0.5 * np.dot(grad, d)
+        return response
 
     def filter_keypoints(
         self,
         dog_images: ndarray,
-        keypoints: ndarray,
         threshold: float = 5.0,
     ) -> ndarray:
         """Filter keypoints using the hessian matrix.
@@ -368,8 +452,9 @@ class SIFT:
         """
         filtered_keypoints = []
         features = []
-        for octave, scale, i, j in keypoints:
-            is_local_max, new_i, new_j, response = self.is_local_maximum(
+        for octave, scale, i, j in self.keypoints_raw:
+            response = self._calculate_pixel_response(octave, scale, i, j)
+            is_local_max = self.is_local_maximum(
                 dog_images,
                 octave,
                 scale,
@@ -382,14 +467,14 @@ class SIFT:
                     dog_images,
                     octave,
                     scale,
-                    new_i,
-                    new_j,
+                    i,
+                    j,
                 )
                 keypoint = KeyPoint()
                 keypoint.response = response
                 keypoint.pt = (
-                    new_j * (2**octave),
-                    new_i * (2**octave),
+                    j * (2**octave),
+                    i * (2**octave),
                 )
                 keypoint.octave = octave + scale * (2**8)
                 keypoint.angle = orientation
@@ -400,8 +485,8 @@ class SIFT:
                 )
                 feature_vector = self.make_sift_descriptor_from_keypoint(
                     dog_images,
-                    new_i,
-                    new_i,
+                    i,
+                    i,
                     octave,
                     scale,
                     orientation,
